b1f8fe6709c5e471ae5241e1fb53fca65d1e6e84
Tim Bolender <git@timbolender.de>
2016-05-26 16:29:40 +0200

    Implemented disambiguation from multiple interpretations.

--- Shrdlite.ts
+++ Shrdlite.ts
@@ -27,28 +27,28 @@ module Shrdlite {
 
 
     /**
-     * Generic function that takes an utterance and returns a plan. It works according to the following pipeline:
-     * - first it parses the utterance (Parser.ts)
-     * - then it interprets the parse(s) (Interpreter.ts)
-     * - then it creates plan(s) for the interpretation(s) (Planner.ts)
-     *
-     * Each of the modules Parser.ts, Interpreter.ts and Planner.ts
-     * defines its own version of interface Result, which in the case
-     * of Interpreter.ts and Planner.ts extends the Result interface
-     * from the previous module in the pipeline. In essence, starting
-     * from ParseResult, each module that it passes through adds its
-     * own result to this structure, since each Result is fed
-     * (directly or indirectly) into the next module.
-     *
-     * There are two sources of ambiguity: a parse might have several
-     * possible interpretations, and there might be more than one plan
-     * for each interpretation. In the code there are placeholders
-     * that you can fill in to decide what to do in each case.
-     *
-     * @param world The current world.
-     * @param utterance The string that represents the command.
-     * @returns A plan in the form of a stack of strings, where each element is either a robot action, like "p" (for pick up) or "r" (for going right), or a system utterance in English that describes what the robot is doing.
-     */
+    * Generic function that takes an utterance and returns a plan. It works according to the following pipeline:
+    * - first it parses the utterance (Parser.ts)
+    * - then it interprets the parse(s) (Interpreter.ts)
+    * - then it creates plan(s) for the interpretation(s) (Planner.ts)
+    *
+    * Each of the modules Parser.ts, Interpreter.ts and Planner.ts
+    * defines its own version of interface Result, which in the case
+    * of Interpreter.ts and Planner.ts extends the Result interface
+    * from the previous module in the pipeline. In essence, starting
+    * from ParseResult, each module that it passes through adds its
+    * own result to this structure, since each Result is fed
+    * (directly or indirectly) into the next module.
+    *
+    * There are two sources of ambiguity: a parse might have several
+    * possible interpretations, and there might be more than one plan
+    * for each interpretation. In the code there are placeholders
+    * that you can fill in to decide what to do in each case.
+    *
+    * @param world The current world.
+    * @param utterance The string that represents the command.
+    * @returns A plan in the form of a stack of strings, where each element is either a robot action, like "p" (for pick up) or "r" (for going right), or a system utterance in English that describes what the robot is doing.
+    */
     export function parseUtteranceIntoPlan(world : World, utterance : string) : string[] {
         // Parsing
         world.printDebugInfo('Parsing utterance: "' + utterance + '"');
@@ -73,6 +73,8 @@ module Shrdlite {
             });
 
             if (interpretations.length > 1) {
+                console.log("inside multiple interpretations",interpretations.length);
+                generateUserQuestion(interpretations,world);
                 // several interpretations were found -- how should this be handled?
                 // should we throw an ambiguity error?
                 // ... throw new Error("Ambiguous utterance");
@@ -112,14 +114,57 @@ module Shrdlite {
         return finalPlan;
     }
 
+    export function generateUserQuestion(interpretations:Interpreter.InterpretationResult[],world :World){
+        var userQuestion : string = "Did you mean ";
+        var firstTime : boolean = true;
+        var erstesMal : boolean = true;
+        var forstaGang : boolean = true;
+        for(var dnf of interpretations){
+            if (!forstaGang) {
+                userQuestion+= "? Or did you mean ";
+            }
+            erstesMal = true;
+            for(var conjList of dnf.interpretation){
+                if (!erstesMal) {
+                    userQuestion+= " or ";
+                }
+                firstTime = true;
+                for(var conj of conjList){
+                    if (!firstTime) {
+                        userQuestion += " and "
+                    }
+                    userQuestion += describeObject(conj.args[0], world);
+                    userQuestion += " to be moved " + conj.relation + " ";
+                    userQuestion += describeObject(conj.args[1], world);
+                    firstTime = false;
+                }
+                erstesMal = false;
+            }
+            forstaGang = false;
+        }
+        userQuestion+="?";
+        throw new Error(userQuestion);
+    }
+
+    export function describeObject(id : string, world : World) : string {
+        var res : string = "the ";
+        if (id === "floor") {
+            res += "floor";
+        } else {
+            res += world.currentState.objects[id].size + " ";
+            res += world.currentState.objects[id].color + " ";
+            res += world.currentState.objects[id].form;
+        }
+        return res;
+    }
 
     /** This is a convenience function that recognizes strings
-     * of the form "p r r d l p r d"
-     */
+    * of the form "p r r d l p r d"
+    */
     export function splitStringIntoPlan(planstring : string) : string[] {
         var plan : string[] = planstring.trim().split(/\s+/);
         var actions : {[act:string] : string}
-            = {p:"Picking", d:"Dropping", l:"Going left", r:"Going right"};
+        = {p:"Picking", d:"Dropping", l:"Going left", r:"Going right"};
         for (var i = plan.length-1; i >= 0; i--) {
             if (!actions[plan[i]]) {
                 return;
